9ba80b21d658e66f58c9d6bfa1103daa781a49dd
Marcin Gren <marcin.gren@gmail.com>
2018-07-22 09:12:28 +0200

    added functionality of scratching out tasks

---
+++ assets/js/todos.js
@@ -0,0 +1,7 @@
+var listOfAllTodos = document.querySelectorAll(".list span")
+
+for (var i = 0; i < listOfAllTodos.length; i++) {
+    listOfAllTodos[i].addEventListener("click", function() {
+        this.classList.add("donetask")
+    })
+}
