7490e88639fa960eaf90a013fda3adc53ac632a6
Marcin Gren <marcin.gren@gmail.com>
2018-07-22 14:36:46 +0200

    added icon

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -4,4 +4,10 @@ for (var i = 0; i < listOfAllTodos.length; i++) {
     listOfAllTodos[i].addEventListener("click", function() {
         this.classList.toggle("donetask")
     })
+    listOfAllTodos[i].addEventListener("mouseover", function() {
+        this.style.backgroundColor = "pink"
+    })
+    listOfAllTodos[i].addEventListener("mouseleave", function() {
+        this.style.backgroundColor = "white"
+    })
 }
