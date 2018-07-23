0b4465475573755ad8acf9d4c4d3bd8fc58d6fbc
Marcin Gren <marcin.gren@gmail.com>
2018-07-24 00:30:06 +0200

    changed nth:child color generation

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,16 +1,16 @@
 var listOfAllTodos = document.querySelectorAll(".holder")
 
 for (var i = 0; i < listOfAllTodos.length; i++) {
-    // listOfAllTodos[i].addEventListener("click", function() {
-        // this.classList.toggle("donetask")
-    // })
     listOfAllTodos[i].addEventListener("click", function() {
+        this.querySelector(".todoitem").classList.toggle("donetask")
+    })
+    listOfAllTodos[i].addEventListener("mouseenter", function() {
         this.querySelector(".bin").classList.remove("hidden")
         this.querySelector(".bin").classList.add("binsignholder")
         console.log("asd")
     })
-    // listOfAllTodos[i].addEventListener("mouseleave", function() {
-    //     this.querySelector("span").classList.add("hidden")
-    //     this.querySelector("span").classList.remove("binsignholder")
-    // })
+    listOfAllTodos[i].addEventListener("mouseleave", function() {
+        this.querySelector(".bin").classList.add("hidden")
+        this.querySelector(".bin").classList.remove("binsignholder")
+    })
 }
