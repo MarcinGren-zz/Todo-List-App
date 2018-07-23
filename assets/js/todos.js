8316c4ac76a2fb77fc31d4641f8f4639fb360d1c
Marcin Gren <marcin.gren@gmail.com>
2018-07-24 00:13:46 +0200

    rearranging divs vs spans, created wrapper, not finished

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,13 +1,16 @@
-var listOfAllTodos = document.querySelectorAll(".list span")
+var listOfAllTodos = document.querySelectorAll(".holder")
 
 for (var i = 0; i < listOfAllTodos.length; i++) {
+    // listOfAllTodos[i].addEventListener("click", function() {
+        // this.classList.toggle("donetask")
+    // })
     listOfAllTodos[i].addEventListener("click", function() {
-        this.classList.toggle("donetask")
+        this.querySelector(".bin").classList.remove("hidden")
+        this.querySelector(".bin").classList.add("binsignholder")
+        console.log("asd")
     })
-    listOfAllTodos[i].addEventListener("mouseover", function() {
-        this.style.backgroundColor = "pink"
-    })
-    listOfAllTodos[i].addEventListener("mouseleave", function() {
-        this.style.backgroundColor = "white"
-    })
+    // listOfAllTodos[i].addEventListener("mouseleave", function() {
+    //     this.querySelector("span").classList.add("hidden")
+    //     this.querySelector("span").classList.remove("binsignholder")
+    // })
 }
